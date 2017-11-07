8574759fd0830cd793011eca64f8b747fc9fd654
Ehddver Cabiten <ehddver.cabiten@valueline.com.ph>
2017-11-07 16:22:09 +0800

    ROOT SCOPE

--- public/js/controllers/main.js
+++ public/js/controllers/main.js
@@ -1,4 +1,7 @@
 app
-.controller("TrainingCtrl",function($scope, $http, $state){
+.controller("TrainingCtrl",function($scope, $http, $state, $rootScope){
 	console.log("APP");
+
+	$rootScope.id = logId;
+
 })
